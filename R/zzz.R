c97a3c6d213c8ea648f3043cd1f3ff491d950014
Jim Thorson <JamesT.esq@gmail.com>
2016-08-08 16:47:00 -0700

    fixed typo in .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,8 @@
 .onAttach <- function(libname, pkgname) {
   print("###########################################################################################")
   print("Loading package FishData, developed by James Thorson for the Northwest Fisheries Science Center")
-  print("This package is in development, and comes with no implied or explicit guaruntee of accuracy"
-  print("Interested users should read the code and colsult the survey-specific websites being queried"
+  print("This package is in development, and comes with no implied or explicit guaruntee of accuracy")
+  print("Interested users should read the code and colsult the survey-specific websites being queried")
   print("###########################################################################################")
   #if( !"ThorsonUtilities" %in% installed.packages()[,1] ){
   #  print("Installing package: ThorsonUtilities...")
