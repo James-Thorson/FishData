6df239d9dac589450db255908eab1945e42c3b94
Jim Thorson <JamesT.esq@gmail.com>
2016-08-08 21:21:05 -0700

    split testing into two files

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,8 +8,12 @@ testthat::test_check("FishData")
 if(FALSE){
   # Run from local directory
   testthat::test_dir( "C:/Users/James.Thorson/Desktop/Project_git/FishData/tests/testthat/", reporter="check" )
+  #testthat::test_dir( "/media/sf_c/Users/jim/Desktop/Project_git/FishData/tests/testthat/", reporter="check" )
+
   # Full build check
   devtools::check( "C:/Users/James.Thorson/Desktop/Project_git/FishData/" )
+  #devtools::check( "/media/sf_c/Users/jim/Desktop/Project_git/FishData/ )
+
   # Update documentation
   devtools::document( "C:/Users/James.Thorson/Desktop/Project_git/FishData/" )
 }
