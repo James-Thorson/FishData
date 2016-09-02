8b830bac46487b8ca8a1ec6f49c00dac346379d4
Jim Thorson <JamesT.esq@gmail.com>
2016-09-01 20:45:10 -0700

    experiment with breaking up unit-tests...
    ... to see if it works better on Travis

--- tests/testthat/test-EBSBTS.R
+++ tests/testthat/test-EBSBTS.R
@@ -15,25 +15,36 @@ test_that("add_missing_zeros is working for EBSBTS ", {
   DF1_fast = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Gadus chalcogrammus", species_colname="Sci", Method="Fast", if_multiple_records="Combine" )
   DF1_slow = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Gadus chalcogrammus", species_colname="Sci", Method="Slow", if_multiple_records="Combine" )
 
+  # Check number of observatiosn
+  expect_equal( length(unique(Data0$TowID)), nrow(DF1_slow), tolerance=1e-10 )
+  expect_equal( length(unique(Data0$TowID)), nrow(DF1_fast), tolerance=1e-10 )
+
+  # Check number of encounters
+  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
+  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
+
+  # Check total biomass by year
+  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=sum), tolerance=1e-10 )
+  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=sum), tolerance=1e-10 )
+})
+test_that("add_missing_zeros is working for EBSBTS ", {
+  # Download data
+  Data0 = download_catch_rates( survey="Eastern_Bering_Sea", add_zeros=FALSE, species_set=Inf )
+  Data = Data0[ which(Data0[,'Sci']=="Gadus chalcogrammus"), ]
+
   # add_missing_zeros -- first
   DF2_fast = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Gadus chalcogrammus", species_colname="Sci", Method="Fast", if_multiple_records="First" )
   DF2_slow = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Gadus chalcogrammus", species_colname="Sci", Method="Slow", if_multiple_records="First" )
 
   # Check number of observatiosn
-  expect_equal( length(unique(Data0$TowID)), nrow(DF1_slow), tolerance=1e-10 )
-  expect_equal( length(unique(Data0$TowID)), nrow(DF1_fast), tolerance=1e-10 )
   expect_equal( length(unique(Data0$TowID)), nrow(DF2_slow), tolerance=1e-10 )
   expect_equal( length(unique(Data0$TowID)), nrow(DF2_fast), tolerance=1e-10 )
 
   # Check number of encounters
-  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
-  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_slow$Wt,INDEX=DF2_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_fast$Wt,INDEX=DF2_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
 
   # Check total biomass by year
-  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=sum), tolerance=1e-10 )
-  expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=sum), tolerance=1e-10 )
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF2_slow$Wt,INDEX=DF2_slow$Year,FUN=sum), tolerance=1e-10 )
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF2_fast$Wt,INDEX=DF2_fast$Year,FUN=sum), tolerance=1e-10 )
 })
--- tests/testthat/test-WCGHL.R
+++ tests/testthat/test-WCGHL.R
@@ -15,27 +15,40 @@ test_that("add_missing_zeros is working for WCGHL ", {
   DF1_fast = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Fast", if_multiple_records="Combine" )
   DF1_slow = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Slow", if_multiple_records="Combine" )
 
-  # add_missing_zeros -- first
-  DF2_fast = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Fast", if_multiple_records="First" )
-  DF2_slow = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Slow", if_multiple_records="First" )
-
   # Check number of observations
   expect_equal( length(unique(Data0$TowID)), nrow(DF1_slow), tolerance=1e-10 )
   expect_equal( length(unique(Data0$TowID)), nrow(DF1_fast), tolerance=1e-10 )
-  expect_equal( length(unique(Data0$TowID)), nrow(DF2_slow), tolerance=1e-10 )
-  expect_equal( length(unique(Data0$TowID)), nrow(DF2_fast), tolerance=1e-10 )
 
   # Check number of encounters
     # Won't work because two are aggregated
   #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
   #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
-  #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_slow$Wt,INDEX=DF2_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
-  #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_fast$Wt,INDEX=DF2_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
 
   # Check total biomass by year
     # Only works when if_multiple_records=="Combine", because if_multiple_records=="First" drops a duplicated TowID
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_slow$Wt,INDEX=DF1_slow$Year,FUN=sum), tolerance=1e-10 )
   expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF1_fast$Wt,INDEX=DF1_fast$Year,FUN=sum), tolerance=1e-10 )
+})
+test_that("add_missing_zeros is working for WCGHL ", {
+  # Download data
+  Data0 = download_catch_rates( survey="WCGHL", add_zeros=FALSE, species_set=Inf )
+  Data = Data0[ which(Data0[,'Sci']=="Sebastes paucispinis"), ]
+
+  # add_missing_zeros -- first
+  DF2_fast = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Fast", if_multiple_records="First" )
+  DF2_slow = add_missing_zeros( data_frame=Data0, unique_sample_ID_colname="TowID", sample_colname="Wt", species_subset="Sebastes paucispinis", species_colname="Sci", Method="Slow", if_multiple_records="First" )
+
+  # Check number of observations
+  expect_equal( length(unique(Data0$TowID)), nrow(DF2_slow), tolerance=1e-10 )
+  expect_equal( length(unique(Data0$TowID)), nrow(DF2_fast), tolerance=1e-10 )
+
+  # Check number of encounters
+    # Won't work because two are aggregated
+  #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_slow$Wt,INDEX=DF2_slow$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
+  #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=function(vec){sum(vec>0)}), tapply(DF2_fast$Wt,INDEX=DF2_fast$Year,FUN=function(vec){sum(vec>0)}), tolerance=1e-10 )
+
+  # Check total biomass by year
+    # Only works when if_multiple_records=="Combine", because if_multiple_records=="First" drops a duplicated TowID
   #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF2_slow$Wt,INDEX=DF2_slow$Year,FUN=sum), tolerance=1e-10 )
   #expect_equal( tapply(Data$Wt,INDEX=Data$Year,FUN=sum), tapply(DF2_fast$Wt,INDEX=DF2_fast$Year,FUN=sum), tolerance=1e-10 )
 })
